7045a967125286e26c8c0467a7e796d1e37bfb3d
cadinkor <elliott.thomlison@gmail.com>
2021-03-25 20:45:36 +0000

    Format adjustments made

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // updates counter in real time
+  $("#tweet-text").on("input", function () {
+    let tweetLength = $(this).val().length;
+    let counter = $(this).parent().find(".counter");
+    counter.text(140 - tweetLength);
+    // counter goes red if character exceed 140
+    if (tweetLength > 140) {
+      counter.addClass("counterRed");
+    } else if (tweetLength <= 140){
+      counter.removeClass("counterRed");
+    }
+  });
+});
