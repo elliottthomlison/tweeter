7329ca220aecec993bf6a36aba2de3a8dbe9ff67
cadinkor <elliott.thomlison@gmail.com>
2021-03-30 01:04:41 +0000

    Adjusting code, adding comments, cleaning up

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
+  
   // updates counter in real time
   $("#tweet-text").on("input", function () {
     let tweetLength = $(this).val().length;
     let counter = $(this).parent().find(".counter");
     counter.text(140 - tweetLength);
+   
     // counter goes red if character exceed 140
     if (tweetLength > 140) {
       counter.addClass("counterRed");
--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 "use strict";
 
 // Basic express setup:
-
-const PORT          = 8080;
+const PORT          = 3001;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
